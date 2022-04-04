fd2cecb6a2e75af2c96cf3e5465e59fcc57111ba
aritro66 <ghosharitro66@gmail.com>
2022-04-04 22:34:49 +0530

    max zoom out to 1

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ window.onload = function () {
     var zoom = canvas.getZoom();
     zoom *= 0.999 ** delta;
     if (zoom > 20) zoom = 20;
-    if (zoom < 0.01) zoom = 0.01;
+    if (zoom < 1) zoom = 1;
     canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
     opt.e.preventDefault();
     opt.e.stopPropagation();
